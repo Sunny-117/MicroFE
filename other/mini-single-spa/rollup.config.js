e86efedf6cdb41902cfd5a5c6d4b6b77982452db
Sunny-117 <zhiqiangfu6@gmail.com>
2024-09-17 18:25:04 +0800

    add readme

--- other/mini-single-spa/rollup.config.js
+++ other/mini-single-spa/rollup.config.js
@@ -1,15 +1,10 @@
-/**
- * @file
- * Created by zhangyatao on 2019/10/21.
- */
-'use strict';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
 import serve from 'rollup-plugin-serve'
 
 export default {
-    input: './src/my-single-spa.js',
+    input: './core/my-single-spa.js',
     output: {
         file: './lib/umd/my-single-spa.js',
         format: 'umd',
